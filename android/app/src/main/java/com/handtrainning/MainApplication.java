718fbfb0647d48fa5f2f7fb925cb72ee5a737737
Filipe <filipecameracastro@gmail.com>
2021-07-21 12:23:52 -0300

    Instalando Pacotes

--- android/app/src/main/java/com/handtrainning/MainApplication.java
+++ android/app/src/main/java/com/handtrainning/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.horcrux.svg.SvgPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
